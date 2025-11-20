f5f8bb41d07148c6e209fd05d7885fa3751e4b54
anago <48195751+auag0@users.noreply.github.com>
2025-11-20 14:28:45 +0900

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "pokego.pip"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
     }
 
     buildTypes {
